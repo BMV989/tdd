eb5be16edb075d5c1f18d7d81d3d0e875869daf5
Maxim <mbrozovskii@gmail.com>
2024-11-17 16:48:14 +0500

    added more tests for CircularCloudLayouterTest

--- cs/TagsCloudVisualization/RandomExtension.cs
+++ cs/TagsCloudVisualization/RandomExtension.cs
@@ -4,7 +4,7 @@ namespace TagsCloudVisualization;
 
 public static class RandomExtension
 {
-    public static Size RandomSize(this Random random, int minValue = 1, int maxValue = int.MaxValue)
+    public static Size RandomSize(this Random random, int minValue, int maxValue)
     {
         if (minValue <= 0)
             throw new ArgumentOutOfRangeException(nameof(minValue), "minValue must be greater than 0");
@@ -13,4 +13,7 @@ public static class RandomExtension
         
         return new Size(random.Next(minValue, maxValue), random.Next(minValue, maxValue));
     }
+    
+    public static Point RandomPoint(this Random random, int minValue, int maxValue) =>
+        new (random.Next(minValue, maxValue), random.Next(minValue, maxValue));
 }
--- cs/TagsCloudVisualizationTests/CircularCloudLayouterTest.cs
+++ cs/TagsCloudVisualizationTests/CircularCloudLayouterTest.cs
@@ -8,22 +8,91 @@ namespace TagsCloudVisualizationTests;
 [TestOf(typeof(CircularCloudLayouter))]
 public class CircularCloudLayouterTest
 {
+    private readonly Random randomizer = new();
+
+    [Test]
+    public void PutNextRectangle_ShouldReturnRectangle()
+    {
+        var circularLayouter = CreateCircularCloudLayouterWithRandomParams();
+        var rectSize = randomizer.RandomSize(1, int.MaxValue);
+        
+        var rect = circularLayouter.PutNextRectangle(rectSize);
+
+        rect.Should().BeOfType<Rectangle>();
+    }
+
+    [Test]
+    public void PutNextRectangle_ShouldReturnRectangleAtCenter_WhenFirstInvoked()
+    {
+        var circularLayouter = CreateCircularCloudLayouterWithRandomParams();
+        var rectSize = randomizer.RandomSize(1, int.MaxValue);
+        
+        var actualRect = circularLayouter.PutNextRectangle(rectSize);
+        var expectedRect = CircularCloudLayouter.CreateRectangle(circularLayouter.Center, rectSize);
+        
+        actualRect.Should().BeEquivalentTo(expectedRect);
+    }
+
+    [Test]
+    public void PutNextRectangle_ShouldReturnRectangle_WithCorrectSize()
+    {
+        var circularLayouter = CreateCircularCloudLayouterWithRandomParams();
+        var recSize = randomizer.RandomSize(1, int.MaxValue);
+        
+        var actualRect = circularLayouter.PutNextRectangle(recSize);
+        
+        actualRect.Size.Should().Be(recSize);
+    }
+    
     [Test]
     public void PutNextRectangle_ShouldReturnRectangles_WithoutIntersections()
     {
-        var randomizer = new Random();
+        var circularCloudLayouter = CreateCircularCloudLayouterWithOptimalParams();
         var numberOfRectangles = randomizer.Next(100, 300);
-        var rectangles = new List<Rectangle>(numberOfRectangles);
-        var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0), 2, 1);
-
-        for (var i = 0; i < numberOfRectangles; i++)
-        {
-            var size = new Size(randomizer.Next(10, 27), randomizer.Next(10, 27));
-            
-            rectangles.Add(circularCloudLayouter.PutNextRectangle(size));
-        }
-
+        
+        var rectangles = Enumerable
+            .Range(0, numberOfRectangles)
+            .Select(_ => circularCloudLayouter.PutNextRectangle(randomizer.RandomSize(10, 27)))
+            .ToList();
+        
         rectangles.Any(fr => rectangles.Any(sr => fr != sr && fr.IntersectsWith(sr)))
             .Should().BeFalse();
+    }
+
+    [Test]
+    public void GeneratedLayout_ShouldHaveHighTightnessAndShapeOfCircularCloud_WithOptimalParams()
+    {
+        const double eps = 0.35;
+        var circularLayouter = CreateCircularCloudLayouterWithOptimalParams();
+        var rectangles = PutRandomRectanglesInLayouter(randomizer.Next(500, 1000), circularLayouter);
+        var layoutSize = GetLayoutSize(rectangles);
+        
+        var diameterOfCircle = Math.Max(layoutSize.Width, layoutSize.Height);
+        var areaOfCircle = Math.PI * Math.Pow(diameterOfCircle, 2) / 4;
+        var areaOfRectangles = (double)rectangles
+            .Select(r => r.Height * r.Width)
+            .Sum();
+        var areaRatio = areaOfCircle / areaOfRectangles;
+
+        areaRatio.Should().BeApproximately(1, eps);
+    }
+    
+    private CircularCloudLayouter CreateCircularCloudLayouterWithOptimalParams() => new(new Point(0, 0));
+    private CircularCloudLayouter CreateCircularCloudLayouterWithRandomParams() => 
+        new(randomizer.RandomPoint(-10, 10), randomizer.Next(1, 10), randomizer.Next(1, 10));
+
+    private List<Rectangle> PutRandomRectanglesInLayouter(int numberOfRectangles, 
+        CircularCloudLayouter circularCloudLayouter) =>
+        Enumerable
+            .Range(0, numberOfRectangles)
+            .Select(_ => circularCloudLayouter.PutNextRectangle(randomizer.RandomSize(10, 27)))
+            .ToList();
+
+    private Size GetLayoutSize(List<Rectangle> rectangles)
+    {
+        var layoutWidth = rectangles.Max(r => r.Right) - rectangles.Min(r => r.Left);
+        var layoutHeight = rectangles.Max(r => r.Top) - rectangles.Min(r => r.Bottom);
+        
+        return new Size(layoutWidth, layoutHeight);
     }
 }

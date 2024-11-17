71ae05052a9eb9852e38a090bde60076a4f252e1
Maxim <mbrozovskii@gmail.com>
2024-11-17 21:11:42 +0500

    small fix in RandomExtensionTest

--- cs/TagsCloudVisualizationTests/RandomExtensionTest.cs
+++ cs/TagsCloudVisualizationTests/RandomExtensionTest.cs
@@ -13,7 +13,7 @@ public class RandomExtensionTest
     
     [TestCase(0, 5, TestName = "MinValue is zero")] 
     [TestCase(-1, 10, TestName = "MinValue is negative")]
-    [TestCase(50, 20, Description = "MinValue is greater than MaxValue")]
+    [TestCase(50, 20, TestName = "MinValue is greater than MaxValue")]
     public void NextSize_ShouldThrowArgumentOutOfRangeException_WithInvalidParams(int minValue, int maxValue)
     {
         Action act = () => random.NextSize(minValue, maxValue);

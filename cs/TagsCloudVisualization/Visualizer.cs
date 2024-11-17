8ff9eb87293551b15f1162eb04b15ac111e6a78f
Maxim <mbrozovskii@gmail.com>
2024-11-17 15:01:15 +0500

    small refactoring of Visualizer

--- cs/TagsCloudVisualization/Visualizer.cs
+++ cs/TagsCloudVisualization/Visualizer.cs
@@ -18,15 +18,14 @@ public class Visualizer
 
     public SKBitmap VisualizeTagCloud(IEnumerable<Rectangle> rectangles)
     {
+        var paint = new SKPaint 
+        { 
+            Color = SKColors.White,
+            Style = SKPaintStyle.Stroke
+        };
+        
         foreach (var rectangle in rectangles)
-        {
-            var skPaint = new SKPaint
-            {
-                Color = SKColors.White,
-                Style = SKPaintStyle.Stroke
-            };
-            canvas.DrawRect(rectangle.ToSKRect(), skPaint);
-        }
+            canvas.DrawRect(rectangle.ToSKRect(), paint);
         
         return bitmap;
     }

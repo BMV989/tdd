009dc35176449055d65afcb69b2f2ad90699a353
Maxim <mbrozovskii@gmail.com>
2024-11-17 17:15:48 +0500

    added tests for RandomExtension

---
+++ cs/TagsCloudVisualizationTests/RandomExtensionTest.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+[TestOf(typeof(RandomExtension))]
+public class RandomExtensionTest
+{
+    private readonly Random random = new();
+    
+    [TestCase(0, 5, TestName = "MinValue is zero")] 
+    [TestCase(-1, 10, TestName = "MinValue is negative")]
+    [TestCase(50, 20, Description = "MinValue is greater than MaxValue")]
+    public void RandomSize_ShouldThrowArgumentOutOfRangeException_WithInvalidParams(int minValue, int maxValue)
+    {
+        Action act = () => random.RandomSize(minValue, maxValue);
+
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+}

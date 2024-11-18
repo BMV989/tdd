687a820da73f9402d3b0c2497ad157dc721c7e53
Maxim <mbrozovskii@gmail.com>
2024-11-18 19:57:32 +0500

    At last!

--- cs/TagsCloudVisualization/TagCloudVisualizer.cs
+++ cs/TagsCloudVisualization/TagCloudVisualizer.cs
@@ -7,7 +7,10 @@ public class TagCloudVisualizer(int width, int height)
 {
     public SKBitmap Visualize(List<SKRect> rectangles)
     {
-        var bitmap = new SKBitmap(width, height);
+        var layoutSize = GetLayoutSize(rectangles);
+        var bimapWidth = Math.Max(width, layoutSize.Width) * 2 ;
+        var bimapHeight = Math.Max(height, layoutSize.Height) * 2;
+        var bitmap = new SKBitmap(bimapWidth,  bimapHeight);
         var canvas = new SKCanvas(bitmap);
         var paint = new SKPaint 
         { 
@@ -17,9 +20,23 @@ public class TagCloudVisualizer(int width, int height)
         
         canvas.Clear(SKColors.White);
         
+         var xOffset = bitmap.Width / 2f - rectangles.First().Location.X;
+         var yOffset = bitmap.Height / 2f - rectangles.First().Location.Y;
+
         foreach (var rectangle in rectangles)
+        {
+            rectangle.Offset(xOffset, yOffset);
             canvas.DrawRect(rectangle, paint);
-        
+        }
+
         return bitmap;
+    }
+    
+    private SKSizeI GetLayoutSize(List<SKRect> rectangles)
+    {
+        var layoutWidth = rectangles.Max(r => r.Right) - rectangles.Min(r => r.Left);
+        var layoutHeight = rectangles.Max(r => r.Top) - rectangles.Min(r => r.Bottom);
+        
+        return new SKSizeI((int)layoutWidth, (int)layoutHeight);
     }
 }

8a64693ec502985847c42b04f1fc9831b46371ef
Maxim <mbrozovskii@gmail.com>
2024-11-18 11:12:48 +0500

    small refactoring of CircularCloudLayouter

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -2,23 +2,16 @@
 
 namespace TagsCloudVisualization;
 
-public class CircularCloudLayouter : ICloudLayouter
+public class CircularCloudLayouter(SKPoint center) : ICloudLayouter
 {
-    private const double OptimalRadius = 1d;
+    private const double OptimalRadius = 1;
     private const double OptimalAngleOffset = 0.5;
     
     private readonly List<SKRect> rectangles = new();
-    private readonly SpiralPointsGenerator pointsGenerator;
-    private readonly SKPoint center;
+    private readonly SpiralPointsGenerator pointsGenerator = new(center, OptimalRadius, OptimalAngleOffset);
     
     public SKPoint Center => center;
     public IEnumerable<SKRect> Rectangles => rectangles;
-    
-    public CircularCloudLayouter(SKPoint center)
-    {
-       this.center = center;
-       pointsGenerator = new SpiralPointsGenerator(center, OptimalRadius, OptimalAngleOffset);
-    }
     
     public SKRect PutNextRectangle(SKSize rectangleSize)
     {
